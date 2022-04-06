6e72b615f765fb200ac2ebb68dba1625fb16a4fa
wibmw <ilaouer@outlook.com>
2022-04-06 18:18:14 +0200

    fields validation

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -9,8 +9,15 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalSubmit = document.querySelector(".btn-submit");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,3 +31,45 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+//verification 2 chart min
+function chart2Min(value) {
+  return /a{2,}$/.test(value);
+}
+
+//disable submit button
+function disableSubmit(disabled) {
+  if (disabled) {
+    modalSubmit.setAttribute("disabled", true);
+  } else {
+    modalSubmit.removeAttribute("disabled");
+  }
+}
+
+//firsname check 2 char
+firstName.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (chart2Min(value)) {
+      isValid = true;
+  } else {
+      isValid = false;
+  }
+});
+
+//lastname check 2 char
+lastName.addEventListener('input', function(e) {
+  var value = e.target.value;
+  if (chart2Min(value)) {
+    disableSubmit(true);
+  } else {
+    disableSubmit(false);
+  }
+});
+
+email.addEventListener('keyup', function (event) {
+  if(email.validity.typeMismatch) {
+    email.setCustomValidity("J'attend un e-mail, mon cher&nbsp;!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
