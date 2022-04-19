5d9d108e84c5f10fe4f3f8c5a65d020ec788186e
wibmw <ilaouer@outlook.com>
2022-04-19 15:32:09 +0200

    Mise en place des messages de validation

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -10,35 +10,38 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");         //modal
 const modalSubmit = document.querySelector(".btn-submit");  //modal submit button
-const modalBtn = document.querySelectorAll(".modal-btn");  //open modal button
-const formData = document.querySelectorAll(".formData");    //modal form
+const modalBtn = document.querySelectorAll(".modal-btn");   //open modal button
+//const formData = document.querySelectorAll(".formData");    //modal form
 const firstName = document.getElementById("first");         //firtname input
 const lastName = document.getElementById("last");           //lastname input
 const email = document.getElementById("email");             //email input
 const birthdate = document.getElementById("birthdate");     //birthdate input
 const quantity = document.getElementById("quantity");       //quantity input
-
+const city = document.getElementById("location1");          //city input
+const conditionAcceptation = document.getElementById("checkbox1");     //conditions input
 
 // default messages
 const firstNameMessage = "Le prénom doit comporter au moins 2 caractères !";
 const lastNameMessage = "Le nom doit comporter au moins 2 caractères !";
 const emailMessage = "L'adresse e-mail n'est pas valide !";
-const birthdateMessage = "La date de naissace n'est pas valide !";
-const quantityMessage = "Merci d'entrer un nombre !";
+const birthdateMessage = "La date de naissance n'est pas valide !";
+const quantityMessage = "Merci d'entrer un nombre entre 1 et 99 !";
+const cityMessage = "Vous devez selectionner une ville !";
+const acceptationMessage =  "Vous devez accepter les conditions d'utilisation !";
+const requiredFieldsMessage =  "Vous devez compléter tous les champs obligatoires !";
 
-// datas validation
-isFirstNameValid = false;
+// datas validation initialisation
+/*isFirstNameValid = false;
 isLastNameValid = false;
 isEmailValid = false;
 isBirthdateValid = false;
 isQuantityValid = false;
-
-// Messages initialisation
-setValidationMessage(firstName,firstNameMessage);
-setValidationMessage(lastName,lastNameMessage);
-setValidationMessage(email, emailMessage);
-setValidationMessage(birthdate, birthdateMessage);
-setValidationMessage(quantity, quantityMessage);
+*/
+firstName.valid = false;
+lastName.valid = false;
+email.valid = false;
+birthdate.valid = false;
+quantity.valid = false;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -53,7 +56,7 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-//verification 2 chart min
+// 2 chart min verification
 function chart2Min(value) {
   console.log(value + "   " + /^[a-zA-Z]{2,}$/.test(value));
   return /^[a-zA-Z]{2,}$/.test(value);
@@ -61,54 +64,43 @@ function chart2Min(value) {
 
 // clear validation message
 function clearValidationMessage(element) {
-  element.setCustomValidity("");
-  element.reportValidity();
+  element.closest(".formData").setAttribute("data-error-visible", "false");
+  element.closest(".formData").setAttribute("data-error", "");
   console.log("clearValidationMessage " + element.name);
 }
 
 // set validation message
 function setValidationMessage(element, message) {
-  element.setCustomValidity(message);
-  element.reportValidity();
+  element.closest(".formData").setAttribute("data-error-visible", "true");
+  element.closest(".formData").setAttribute("data-error", message);
   console.log("setValidationMessage " + element.name);
 }
 
-//********************* CHECK MESSAGES  ***********************************/
-//firstname check message
-firstName.addEventListener('keyup', function (event) {
-  if (!(chart2Min(firstName.value)) || firstName === '') {
-    setValidationMessage(firstName, firstNameMessage);
-    isFirstNameValid = false;
+//********************* CHECK FUNCTIONS  ***********************************/
+// check names function
+function namesCheck(name, message){
+  if (!(chart2Min(name.value)) || name === '') {
+    setValidationMessage(name, message);
+    name.valid = false;
   } else {
-    clearValidationMessage(firstName);
-    isFirstNameValid = true;
+    clearValidationMessage(name);
+    name.valid = true;
   }
-});
+}
 
-//lastnamecheck message
-lastName.addEventListener('keyup', function (event) {
-  if (!(chart2Min(lastName.value)) || lastName === '') {
-    setValidationMessage(lastName, lastNameMessage);
-    isLastNameValid = false;
-  } else {
-    clearValidationMessage(lastName);
-    isLastNameValid = true;
-  }
-});
-
-//email check message
-email.addEventListener('change', function (event) {
+// check email function
+function emailCheck(email, message){
   if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(email.value)) || email === '') {
-    setValidationMessage(email, emailMessage);
-    isEmailValid = false;
+    setValidationMessage(email, message);
+    email.valid = false;
   } else {
     clearValidationMessage(email);
-    isEmailValid = true;
+    email.valid = true;
   }
-});
+}
 
-//birthdate check message
-birthdate.addEventListener('change', function (event) {
+// check birthdate function
+function birthdateCheck(birthdate, message){
   today = new Date(Date.now());
   today.setFullYear( today.getFullYear() - 10 );
   minDate = new Date(Date.parse("1900/01/01"));
@@ -116,49 +108,78 @@ birthdate.addEventListener('change', function (event) {
  
   if ((selectedDate < today ) && (selectedDate > minDate)  && (selectedDate > minDate)) {
     clearValidationMessage(birthdate);
-    isBirthdateValid = true;
+    birthdate.valid = true;
   } else {
-    console.log(birthdate.value);
-    setValidationMessage(birthdate, birthdateMessage);
-    isBirthdateValid = false;
+    setValidationMessage(birthdate, message);
+    birthdate.valid = false;
   }
-});
+}
 
-//quantity check message
-quantity.addEventListener('keyup', function (event) {
-  console.log(quantity.validity.typeMismatch)
+// check quantity function
+function quantityCheck(quantity, message){
   if((quantity.value < 1) || (quantity.value > 99)) {
-    setValidationMessage(quantity, quantityMessage);
-    isQuantityValid = false;
+    setValidationMessage(quantity, message);
+    quantity.valid = false;
   } else {
     clearValidationMessage(quantity);
-    isQuantityValid = true;
+    quantity.valid = true;
   }
+}
+
+//********************* EVENT CHECK MESSAGES  ***********************************/
+//firstname event message
+firstName.addEventListener('keyup', function (event) {
+  namesCheck(firstName, firstNameMessage);
 });
 
-//form fields validation
+//lastname event message
+lastName.addEventListener('keyup', function (event) {
+  namesCheck(lastName, lastNameMessage);
+});
+
+//email event message
+email.addEventListener('change', function (event) {
+  emailCheck(email, emailMessage);
+});
+
+//birthdate event message
+birthdate.addEventListener('change', function (event) {
+  birthdateCheck(birthdate, birthdateMessage);
+});
+
+//quantitye event message
+quantity.addEventListener('keyup', function (event) {
+  quantityCheck(quantity, quantityMessage);
+});
+
+//********************* FORM VALIDATION  ***********************************/
 function formValidation(e) {
   const radios = document.querySelectorAll("input[name='location']:checked");  //radios checkbox
-  if(isFirstNameValid && isLastNameValid && isEmailValid && isBirthdateValid && isQuantityValid){
+  // fields check
+  namesCheck(firstName, firstNameMessage);
+  namesCheck(lastName, lastNameMessage);
+  emailCheck(email, emailMessage);
+  birthdateCheck(birthdate, birthdateMessage);
+  quantityCheck(quantity, quantityMessage);
+
+  if(firstName.valid && lastName.valid && email.valid && birthdate.valid && quantity.valid){
+    clearValidationMessage(modalSubmit);
     if(!(radios.length)) {
-      //setValidationMessage(document.getElementById("location1"), "Vous devez selectionner une ville !");
-      console.log("not good");
+      setValidationMessage(city, cityMessage);
       return false;
-  
     } else {
-      //clearValidationMessage(document.getElementById("location1"));
-      console.log("good");
+      clearValidationMessage(city);
         //Termes Acceptation
         if(checkbox1.checked){
-          console.log("checked");
+          clearValidationMessage(conditionAcceptation);
           return true;
         }else{
-          console.log("notchecked");
+          setValidationMessage(conditionAcceptation, acceptationMessage);
           return false;
         }
     }
   }else{
-    console.log("form not valid");
+    setValidationMessage(modalSubmit, requiredFieldsMessage);
     return false;
   }
 };
